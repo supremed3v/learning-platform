8d7c74c131155496d0ca1c4397103dbdaea9a509
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-03-01 20:32:56 +0500

    local server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+server.on("error", (error) => {
+  console.log("Server error: ", error.message);
+});
