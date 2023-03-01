5d105c416a0270ae1c580006744549b4b1593d0a
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-03-01 20:47:18 +0500

    completed server setup

--- server/server.js
+++ server/server.js
@@ -1,24 +1,45 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cloudinary from "cloudinary";
 import connectDB from "./utils/connectDB.js";
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const PORT = process.env.PORT || 8080;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-connectDB();
+process.on("uncaughtException", (error) => {
+  console.log("Uncaught Exception: ", error.message);
+  console.log("Shutting down server due to uncaught exception");
+  process.exit(1);
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-server.on("error", (error) => {
-  console.log("Server error: ", error.message);
+process.on("unhandledRejection", (error) => {
+  console.log("Unhandled Rejection: ", error.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("ValidationError", (error) => {
+  console.log("Validation Error: ", error.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
