a40bf14a2d8841578a90401d475f8f9bcd7ce955
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-03-11 01:07:46 +0500

    added student and instructor registration page

---
+++ client/src/pages/auth/instructor-register.jsx
@@ -0,0 +1,14 @@
+import { Layout } from "@/components";
+import React from "react";
+
+const InstructorRegister = () => {
+  return (
+    <Layout criteria={false}>
+      <div className="container px-20 ml-10 py-40 pt-20 mx-auto">
+        <h1>Instructor Register Page</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default InstructorRegister;
