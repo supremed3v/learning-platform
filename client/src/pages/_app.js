c7721d96b84b8208f8079bed3ee36f919428db00
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-03-24 17:58:23 +0500

    linked BE stripe with client side

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -4,6 +4,8 @@ import "@/styles/globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import Layout from "@/components/Layout";
 
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 export default function App({ Component, pageProps }) {
   return (
     <>
